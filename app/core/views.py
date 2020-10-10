fb068ec75678b48b2f43a3aa959768875aed9f70
nikita220800 <nikita220800@mail.ru>
2020-10-10 23:33:25 +0300

    core and api changes

--- app/core/views.py
+++ app/core/views.py
@@ -1,10 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-from rest_framework import viewsets
-from .serializers import ArticleSerializer
-from .models import Article
 
-
-class ArticleViewSet(viewsets.ModelViewSet):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
+def index(request):
+    return render(request, template_name='index.html')
