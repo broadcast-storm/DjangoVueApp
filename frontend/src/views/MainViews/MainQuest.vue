b1845428f93f1a5de348b9e7bdb434e480cb8153
nikita220800 <nikita220800@mail.ru>
2020-10-19 14:26:04 +0300

    test using d2 charts

--- frontend/src/views/MainViews/MainQuest.vue
+++ frontend/src/views/MainViews/MainQuest.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>Контент основного квеста</div>
+    <div class="main-quest">
+        <div class="main-quest__container">
+            <QuestTree />
+        </div>
+    </div>
 </template>
 
 <script>
+import QuestTree from '@/components/QuestTree'
 export default {
     name: 'MainQuest',
+    components: { QuestTree },
     props: {},
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-quest {
+    &__container {
+        max-width: 1100px;
+    }
+}
+</style>
