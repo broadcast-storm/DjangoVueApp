f174a91a5b084961b75f90b4499e21840b05f178
nikita220800 <nikita220800@mail.ru>
2020-12-02 01:00:54 +0300

    build with shop page

---
+++ frontend/dist/static/js/chunk-2d0c45ea.dfe3bb92.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c45ea"],{"3b16":function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" Контент тестов ")])},c=[],a={name:"Tests",props:{}},r=a,u=t("2877"),o=Object(u["a"])(r,s,c,!1,null,"5be597e4",null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0c45ea.dfe3bb92.js.map
