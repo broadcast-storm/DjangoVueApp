b701645e2a09a69a6b5ed5113279b44ae751078f
nikita220800 <nikita220800@mail.ru>
2021-04-13 13:14:58 +0300

    simple jwt tokens with blacklist done, test cron job done

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import JobPositionViewSet, DivisionViewSet, UserProfileViewSet, StatisticsViewSet, \
-                   TaskViewSet, WeeklyTaskViewSet, TeamsViewSet, login, refresh_token, logout, competition
+    TaskViewSet, WeeklyTaskViewSet, TeamsViewSet, LogoutView, LogoutAllView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'job-positions', JobPositionViewSet)
@@ -14,10 +19,15 @@ router.register(r'weekly-tasks', WeeklyTaskViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('login', login, name='login'),
-    path('refresh-token', refresh_token, name='refresh-token'),
-    path('logout', logout, name='logout'),
-    path('competition', competition, name = 'competition'),
-    # path('competition/currentcompetitions', currentcompetitions, name = 'currentcompetitions')
-    # path('searchcompetitions')
+    path('login', TokenObtainPairView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='auth_logout'),
+    path('logout-all', LogoutAllView.as_view(), name='auth_logout_all'),
+    path('refresh-token', TokenRefreshView.as_view(), name='refresh-token'),
+    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('register/', RegisterView.as_view(), name='auth_register'),
+    # path('change_password/<int:pk>/', ChangePasswordView.as_view(),
+    #      name='auth_change_password'),
+    # path('update_profile/<int:pk>/', UpdateProfileView.as_view(),
+    #      name='auth_update_profile'),
 ]
