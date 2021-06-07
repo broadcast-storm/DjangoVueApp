c8609914baa48507c9f0ee73f829509ed286b2ea
Nikita Pozdnyakov <nikita220800@mail.ru>
2021-06-07 14:42:16 +0300

    update urls

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.urls import path, include, re_path
@@ -37,16 +38,17 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^baton/', include('baton.urls')),
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^api/', include('api.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
-            cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
-    re_path(r'^.*$', index, name='index'),
+    url(r'^baton/', include('baton.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('api.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+                                           cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+                                         cache_timeout=0), name='schema-redoc'),
+    # re_path(r'^.*$', index, name='index'),
+    url(r'^.*$', index, name='index'),
 ]
 
 if settings.DEBUG:
