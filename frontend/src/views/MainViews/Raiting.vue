34e7abe3bb4eebc80aa9ab287e0a5f1193575a7d
qoterwoter <marderics@gmail.com>
2021-10-14 01:04:21 +0300

    layout fix

--- frontend/src/views/MainViews/Raiting.vue
+++ frontend/src/views/MainViews/Raiting.vue
@@ -3,10 +3,8 @@
         <main class="main">
             <div class="rating-menu">
                 <div class="command-type">
-                    <!-- <h3 class='selected'>Индивидуальный</h3> -->
-                    <router-link to="/individual">Индивидуальный</router-link>
-                    <router-link to="/command">Командный</router-link>
-                    <!-- <h3>Командный</h3> -->
+                    <router-link class='command-type-link command-type-link__active' to="/individual">Индивидуальный</router-link>
+                    <router-link class='command-type-link' to="/command">Командный</router-link>
                 </div>
                 <div class="period">
                     <span class="period__arrow">
@@ -19,58 +17,29 @@
                 </div>
             </div>
             <router-view></router-view>
-            <!-- БЛОК ИНДИВИДУАЛЬНОГО РЕЙТИНГА -->
+            <!-- БЛОК РЕЙТИНГА -->
             <div class="rating-table">
-                <div class="row-individual">
+                <div class="row">
                     <div
                         v-for="raiting in individualRaiting"
                         :key="raiting.id"
-                        class="row-individual__item"
+                        class="row__item"
                     >
-                        <div class="row-individual__num">{{ raiting.id }}</div>
-                        <div class="row-individual__info">
+                        <div class="row__num">{{ raiting.id }}</div>
+                        <div class="row__info">
                             <img
-                                class="row-individual__img"
+                                class="row__img"
                                 :src="raiting.img"
                                 alt=""
                             />
-                            <div class="row-individual__wrap">
-                                <h4 class="row-individual__title">
+                            <div class="row__wrap">
+                                <h4 class="row__title">
                                     {{ raiting.name }}
                                 </h4>
-                                <span class="row-individual__description"
+                                <span class="row__description"
                                     >Отдел: {{ raiting.description }}</span
                                 >
-                                <p class="row-individual__raiting">
-                                    Рейтинг
-                                    {{ toNumberString(raiting.raitingValue) }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- БЛОК КОМАНДНОГО РЕЙТИНГА -->
-            <div class="rating-table">
-                <div class="row-command">
-                    <div
-                        v-for="raiting in teamRaiting"
-                        :key="raiting.id"
-                        class="row-command__item"
-                    >
-                        <div class="row-command__num">{{ raiting.id }}</div>
-                        <div class="row-command__info">
-                            <img
-                                class="row-command__img"
-                                :src="raiting.img"
-                                alt=""
-                            />
-                            <div class="row-command__wrap">
-                                <h4 class="row-command__title">
-                                    {{ raiting.name }}
-                                </h4>
-                                <p class="row-command__raiting">
+                                <p class="row__raiting">
                                     Рейтинг
                                     {{ toNumberString(raiting.raitingValue) }}
                                 </p>
@@ -87,77 +56,6 @@
 import PeriodArrowSvg from '@/assets/icons/raiting/period-arrow.svg'
 import { mapGetters } from 'vuex'
 
-const individual = {
-    template: `
-    <div class="rating-table">
-                <div class="row-individual">
-                    <div
-                        v-for="raiting in individualRaiting"
-                        :key="raiting.id"
-                        class="row-individual__item"
-                    >
-                        <div class="row-individual__num">{{ raiting.id }}</div>
-                        <div class="row-individual__info">
-                            <img
-                                class="row-individual__img"
-                                :src="raiting.img"
-                                alt=""
-                            />
-                            <div class="row-individual__wrap">
-                                <h4 class="row-individual__title">
-                                    {{ raiting.name }}
-                                </h4>
-                                <span class="row-individual__description">Отдел: {{
-                                    raiting.description
-                                }}</span>
-                                <p class="row-individual__raiting">
-                                    Рейтинг
-                                    {{ toNumberString(raiting.raitingValue) }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    `,
-}
-const command = {
-    template: `
-    <div class="rating-table">
-                <div class="row-command">
-                    <div
-                        v-for="raiting in teamRaiting"
-                        :key="raiting.id"
-                        class="row-command__item"
-                    >
-                        <div class="row-command__num">{{ raiting.id }}</div>
-                        <div class="row-command__info">
-                            <img
-                                class="row-command__img"
-                                :src="raiting.img"
-                                alt=""
-                            />
-                            <div class="row-command__wrap">
-                                <h4 class="row-command__title">
-                                    {{ raiting.name }}
-                                </h4>
-                                <p class="row-command__raiting">
-                                    Рейтинг
-                                    {{ toNumberString(raiting.raitingValue) }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    `,
-}
-
-const routes = [
-    { path: '/', component: individual },
-    { path: '/command', component: command },
-]
-routes
 export default {
     name: 'Raiting',
     components: { PeriodArrowSvg },
@@ -211,11 +109,13 @@ export default {
         justify-content: space-between;
         color: #545969;
         font-size: 24px;
-        h3 {
-            padding-bottom: 10px;
+        .command-type-link {
+            padding-bottom: 8px;
+            color: #545969;
+            text-decoration: none;
         }
-        .selected {
-            border-bottom: 3px solid #545969;
+        .command-type-link__active {
+            border-bottom: 2px solid #545969;
         }
     }
 }
@@ -224,33 +124,18 @@ export default {
     margin-right: 14px;
     // height: 100%;
     // overflow-y: auto;
-    .row-individual__item:nth-child(1) {
-        .row-individual__num {
+    .row__item:nth-child(1) {
+        .row__num {
             background-color: #ebe31d;
         }
     }
-    .row-individual__item:nth-child(2) {
-        .row-individual__num {
+    .row__item:nth-child(2) {
+        .row__num {
             background-color: #99a4a5;
         }
     }
-    .row-individual__item:nth-child(3) {
-        .row-individual__num {
-            background-color: #be8e10;
-        }
-    }
-    .row-command__item:nth-child(1) {
-        .row-command__num {
-            background-color: #ebe31d;
-        }
-    }
-    .row-command__item:nth-child(2) {
-        .row-command__num {
-            background-color: #99a4a5;
-        }
-    }
-    .row-command__item:nth-child(3) {
-        .row-command__num {
+    .row__item:nth-child(3) {
+        .row__num {
             background-color: #be8e10;
         }
     }
@@ -286,7 +171,7 @@ export default {
     }
 }
 
-.row-individual {
+.row {
     &__item {
         display: flex;
         max-height: 88px;
@@ -339,62 +224,6 @@ export default {
 
     &__raiting {
         margin-top: 7px;
-    }
-}
-.row-command {
-    &__item {
-        display: flex;
-        max-height: 88px;
-        max-width: 75%;
-        margin: 17px 0;
-        background: #fff;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    }
-
-    &__num {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: #26bcc2;
-        color: #fff;
-        font-size: 64px;
-        width: 81px;
-        height: 88px;
-    }
-
-    &__info {
-        display: flex;
-        flex: 1;
-        // padding: 5px;
-        // padding-left: 8px;
-    }
-
-    &__img {
-        // max-width: 81px;
-        // max-height: 78px;
-        margin-right: 11px;
-    }
-
-    &__title,
-    &__raiting {
-        font-size: 18px;
-        line-height: 16px;
-        color: #7d849a;
-    }
-
-    &__title {
-        padding-top: 8px;
-        color: #1a2740;
-    }
-
-    &__description {
-        font-size: 12px;
-        line-height: 25px;
-        color: #7d849a;
-    }
-
-    &__raiting {
-        margin-top: 35px;
     }
 }
 </style>
